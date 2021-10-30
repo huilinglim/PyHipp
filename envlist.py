75e53f98d51c99968822770d618aa7dffb4a5b24
AWS ParallelCluster user <ec2-user@ip-10-0-12-211.ap-southeast-1.compute.internal>
2021-10-30 17:23:28 +0000

    add envlist.py script

--- envlist.py
+++ envlist.py
@@ -40,21 +40,24 @@ lock = FileLock(lock_path, timeout=time_out_secs)
 
 with lock:
     if pmode == RESET_MODE:
-        # create a list (named clist) of nevns environments with the 
+        # create a list (named clist) of nevns environments with the
         # prefix envprefix
         # add code here
+        clist=[]
+        for i in range(int(nenvs)):
+            clist.append(envprefix+str(i))
     else:
         # load hickle file
         clist = hickle.load(file_path)
 
         if pmode == WRITE_MODE:
             # append item to end of list
-            # add code here
-        else:    
+            clist.append(env)
+        else:
             # get and remove env from clist
-            # add code here
+            env=clist[0]
+            clist.pop(0)
             # return env name
             print(env)
-
     # save hickle file
     hickle.dump(clist, file_path, mode="w")
